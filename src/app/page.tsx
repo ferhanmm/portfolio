7146a799deea399bda05b5e8603f5198625d7962
ferhanmm <ferhanmm@gmail.com>
2025-08-05 22:47:38 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
       <div className="w-96 h-56 bg-white shadow-md overflow-hidden font-['IBM_Plex_Mono']">
-          <div className="flex flex-col md:flex-row h-full">
+          <div className="flex flex-row h-full">
 
               {/* Left Section (1/3) */}
-              <div className="md:w-1/3 p-4 flex items-center justify-center">
+              <div className="w-1/3 p-4 flex items-center justify-center">
                 <span className="text-5xl font-regular">FM</span>
               </div>
 
               {/* Right Section (2/3) */}
-              <div className="md:w-2/3 flex flex-col justify-center">
+              <div className="w-2/3 flex flex-col justify-center">
                 <div>
                   <span className="text-md font-medium block">Ferhan Moton</span>
                   <span className="text-xs font-regular italic block">IAM Engineer</span>
