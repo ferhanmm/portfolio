f2472675fc0a38ddf931cd2a18060007a9cebac0
ferhanmm <ferhanmm@gmail.com>
2025-08-05 20:18:46 -0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['placeholderimage.org'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
